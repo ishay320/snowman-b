08eb588f6b3e088fcc15120d5b57883b45f7e3f3
ishay320 <ishay320@hotmail.com>
2021-03-15 21:22:01 +0200

    done

--- snowman.cpp
+++ snowman.cpp
@@ -1,17 +1,18 @@
 #include "snowman.hpp"
+#include <array>
 #include <stdexcept>
 #include <string>
-#include <array>
+
 namespace ariel {
 namespace snowmanBody {
-std::array<std::string,4> hat = {{"\n _===_\n", "  ___\n .....\n", "   _\n  /_\\\n",
-                      "  ___\n (_*_)\n"}};             // H
-std::array<std::string,4> nose = {{",", ".", "_", " "}};           // N
-std::array<std::string,4> eye = {{".", "o", "O", "-"}};            // L/R
-std::array<std::string,4> LArm = {{" <", "\\ ", " /", "  "}};      // X
-std::array<std::string,4> RArm = {{" >", "/ ", " \\", "  "}};      // Y
-std::array<std::string,4> torso = {{" : ", "] [", "> <", "   "}};  // T
-std::array<std::string,4> base = {{" : ", "\" \"", "___", "   "}}; // B
+std::array<std::string, 4> hat = {{"\n _===_\n", "  ___\n .....\n",
+                                   "   _\n  /_\\\n", "  ___\n (_*_)\n"}}; // H
+std::array<std::string, 4> nose = {{",", ".", "_", " "}};                 // N
+std::array<std::string, 4> eye = {{".", "o", "O", "-"}};                  // L/R
+std::array<std::string, 4> LArm = {{" <", "\\ ", " /", "  "}};            // X
+std::array<std::string, 4> RArm = {{" >", "/ ", " \\", "  "}};            // Y
+std::array<std::string, 4> torso = {{" : ", "] [", "> <", "   "}};        // T
+std::array<std::string, 4> base = {{" : ", "\" \"", "___", "   "}};       // B
 } // namespace snowmanBody
 void checkIfOK(int build) {
   const int MIN_INPUT = 11111111;
@@ -21,12 +22,13 @@ void checkIfOK(int build) {
     throw e;
   }
   const int MAX_LOOP = 100000000;
-  const int TEN =10;
+  const int TEN = 10;
   const int MAX_SINGLE_NUM = 5;
   const int MIN_SINGLE_NUM = 0;
 
   for (int i = 1; i < MAX_LOOP; i *= TEN) {
-    if (!((build / i) % TEN < MAX_SINGLE_NUM && (build / i) % TEN > MIN_SINGLE_NUM)) {
+    if (!((build / i) % TEN < MAX_SINGLE_NUM &&
+          (build / i) % TEN > MIN_SINGLE_NUM)) {
       std::exception e;
       throw e;
     }
@@ -35,14 +37,14 @@ void checkIfOK(int build) {
 using namespace snowmanBody;
 std::string snowman(int build) {
   checkIfOK(build);
-  const int HAT_POS =10000000;
-  const int LARM_POS =1000;
-  const int LEYE_POS =100000;
-  const int REYE_POS =10000;
-  const int NOSE_POS =1000000;
-  const int RARM_POS =100;
-  const int TORSO_POS =10;
-  const int BASE_POS =1;
+  const int HAT_POS = 10000000;
+  const int LARM_POS = 1000;
+  const int LEYE_POS = 100000;
+  const int REYE_POS = 10000;
+  const int NOSE_POS = 1000000;
+  const int RARM_POS = 100;
+  const int TORSO_POS = 10;
+  const int BASE_POS = 1;
   const int TEN = 10;
   std::string snowman = hat.at(build / HAT_POS - 1);
   snowman += LArm.at((build / LARM_POS - 1) % TEN).at(0);
